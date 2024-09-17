7b34c269cd5458b198fb96a92c8ae95e8c8287f5
Anjana <anjanapcollege@gmail.com>
2024-09-16 20:17:39 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Anjana Rajamani on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
